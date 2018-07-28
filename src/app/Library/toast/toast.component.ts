682ead02a66f9771b1b251666f26c9658250caf1
Gao Fan <gaofan19901103@gmail.com>
2018-07-28 12:34:00 +0800

    add toast

---
+++ src/app/Library/toast/toast.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector:'toast',
+    templateUrl:'./src/app/Library/toast/toast.component.html',
+    styleUrls:['./src/app/Library/toast/toast.component.css']
+})
+
+export class ToastComponent{
+
+}
