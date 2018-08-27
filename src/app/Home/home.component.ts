27699e29b062ec89fbe936c1cdb2d514a5d77391
Gao Fan <gaofan19901103@gmail.com>
2018-08-27 23:50:58 +0800

    refactor header

--- src/app/Home/home.component.ts
+++ src/app/Home/home.component.ts
@@ -2,10 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector:'home',
-    template:`<h1>home not home app</h1><gf-icon iconId="education"></gf-icon>
-        <gf-icon iconId="skill"></gf-icon>
-        <gf-icon iconId="tool"></gf-icon>
-    `
+    templateUrl:'home.component.html'
 })
 export class HomeComponent{
 
