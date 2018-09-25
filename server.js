32aeb75da22e043e2a6673169d237a9d2f4e0698
Fan Gao <fan.gao@thomsons.com>
2018-09-25 18:36:17 +0800

    dowload, I don't understand responseType

--- server.js
+++ server.js
@@ -6,81 +6,98 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((request, response) => {
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'text/html');
-//   var html = fs.readFileSync('index.html');
-//   res.end(html);
-
-      var filePath = '.' + request.url;
-    if (filePath == './')
-        filePath = './src/index.html';
-
-    var extname = path.extname(filePath);
-
-    let current = __dirname;
-    let currentRePath = path.join(__dirname, "src");
-    if(extname == '.jpg' || extname == '.svg' ||  extname == '.png'){
-        //debugger;
-        let diffParts = request.url.split('/');
-        filePath = path.join("./","resource",diffParts[diffParts.length - 1]);
+    if(request.method === 'POST' && request.url == "/downloadresume"){
+        handler.downloadHandler(request, response);
     }
-
-    var contentType = 'text/html';
-    switch (extname) {
-        case '.html':
-            contentType = 'text/html';
-            break;
-        case '.js':
-            contentType = 'text/javascript';
-            break;
-        case '.css':
-            contentType = 'text/css';
-            break;
-        case '.json':
-            contentType = 'application/json';
-            break;
-        case '.png':
-            contentType = 'image/png';
-            break;      
-        case '.jpg':
-            contentType = 'image/jpg';
-            break;
-        case '.wav':
-            contentType = 'audio/wav';
-            break;
-        case '.svg':
-            contentType = 'image/svg+xml';
-            break;
-        case '.ico':
-            contentType = 'image/x-icon';
-            break;
+    else if (request.method === 'GET') {
+        handler.staticFileHandler(request, response);
     }
+});
+
+var handler = {
+    staticFileHandler: function handleStaticFile(request, response){
+        var filePath = '.' + request.url;
+        if (filePath == './')
+            filePath = './src/index.html';
     
-    // var content = fs.readdirSync(filePath);
-    // response.writeHead(200, { 'Content-Type': contentType });
-    // response.end(content, 'utf-8');
-
-    fs.readFile(filePath, function(error, content) {
-        if (error) {
-            if(error.code == 'ENOENT'){
-                fs.readFile('./404.html', function(error, content) {
-                    response.writeHead(200, { 'Content-Type': contentType });
-                    response.end(content, 'utf-8');
-                });
+        var extname = path.extname(filePath);
+        var contentType = 'text/html';
+    
+        if(extname == '.jpg' || extname == '.svg' ||  extname == '.png'){
+            let segments = request.url.split('/');
+            let resourceName = segments[segments.length - 1];
+            filePath = path.join("./","resource",resourceName);
+        }
+        
+        switch (extname) {
+            case '.html':
+                contentType = 'text/html';
+                break;
+            case '.js':
+                contentType = 'text/javascript';
+                break;
+            case '.css':
+                contentType = 'text/css';
+                break;
+            case '.json':
+                contentType = 'application/json';
+                break;
+            case '.png':
+                contentType = 'image/png';
+                break;      
+            case '.jpg':
+                contentType = 'image/jpg';
+                break;
+            case '.wav':
+                contentType = 'audio/wav';
+                break;
+            case '.svg':
+                contentType = 'image/svg+xml';
+                break;
+            case '.ico':
+                contentType = 'image/x-icon';
+                break;
+        }
+        
+        fs.readFile(filePath, function(error, content) {
+            if (error) {
+                if(error.code == 'ENOENT'){
+                    fs.readFile('./404.html', function(error, content) {
+                        response.writeHead(200, { 'Content-Type': contentType });
+                        response.end(content, 'utf-8');
+                    });
+                }
+                else {
+                    response.writeHead(500);
+                    response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+                    response.end(); 
+                }
             }
             else {
+                response.writeHead(200, { 'Content-Type': contentType });
+                response.end(content, 'utf-8');
+            }
+        });
+    },
+    downloadHandler: function handleDownLoad(request, response){
+        let filePath = path.join("./","resource","resume.pdf");
+        
+        fs.readFile(filePath, function(error, content) {
+            if (error) {       
                 response.writeHead(500);
                 response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
-                response.end(); 
+                response.end();              
             }
-        }
-        else {
-            response.writeHead(200, { 'Content-Type': contentType });
-            //response.setHeader('Content-Type', 'text/html');
-            response.end(content, 'utf-8');
-        }
-    });
-});
+            else {
+                // response.setHeader('Content-disposition', 'attachment; filename='+ 'myfile.pdf');
+                // response.end(content, 'application/pdf');
+                //var buffer = Buffer.from(content);
+                //var buffer = Blob.from(content);
+                response.end(content,'application/octet-stream');
+            }
+        });
+    }
+};
 
 server.listen(port, hostname, () => {
   console.log(`Dev Server running at http://${hostname}:${port}/`);
--- src/app/Resume/resume.component.ts
+++ src/app/Resume/resume.component.ts
@@ -1,12 +1,14 @@
 import {Component, OnInit} from '@angular/core';
+import { Http, Headers, RequestOptionsArgs, ResponseContentType } from '@angular/http';
 
 @Component({
     selector:'resume',
     templateUrl:'resume.component.html'
 })
 export class ResumeComponent implements OnInit{
+    responseContentType = ResponseContentType;
 
-    constructor(){}
+    constructor(private http: Http){}
 
     ngOnInit(){
     }
@@ -14,6 +16,45 @@ export class ResumeComponent implements OnInit{
     testmethod(ele: HTMLElement){
         window.alert('haha');
         
+    }
+
+    download(){
+        this.http.post('downloadresume',null, <RequestOptionsArgs>{headers: this.createHttpCallHeaders(), responseType: ResponseContentType.Blob }).map((response) =>{
+            return response;
+        }).subscribe((data) =>{
+            let x = 0;
+            var headers = data.headers.values();
+            //var blob = new Blob([data.arrayBuffer()],{type:"application/pdf"});
+            var blob = new Blob([data.blob()],{type:"application/pdf"});
+            var link = document.createElement('a');
+            link.href = window.URL.createObjectURL(blob);
+            link.download = "Filename.pdf";
+            link.click();
+        });
+
+        // var oReq = new XMLHttpRequest();
+        // oReq.open("GET", "/downloadresume", true);
+        // oReq.responseType = "blob";
+
+        // oReq.onload = function(oEvent) {
+        //     var blobData = oReq.response;
+        //     var blob = new Blob([blobData],{type:"application/pdf"});
+        //     var link = document.createElement('a');
+        //     link.href = window.URL.createObjectURL(blob);
+        //     link.download = "Filename.pdf";
+        //     link.click();
+        // };
+
+        // oReq.send();
+    }
+
+    private createHttpCallHeaders() {
+        let headers = new Headers();
+
+
+        headers.append('Content-Type', 'application/download');
+
+        return headers;
     }
 }
 
